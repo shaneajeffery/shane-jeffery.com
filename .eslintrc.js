e678236270e4f355d87d35af5296bab3b38cac12
Shane Jeffery <1stphormula@gmail.com>
2021-04-15 21:51:41 -0700

    Fix eslint issues and prettier as well.

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,11 +17,10 @@ module.exports = {
         'react-app',
         'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
         'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-        'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
         'plugin:prettier/recommended',
         'eslint:recommended',
     ],
-    plugins: ['simple-import-sort'],
+    plugins: [],
     settings: {
         react: {
             version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
@@ -31,7 +30,6 @@ module.exports = {
         // Rules that start as warning and could be error in the future
         '@typescript-eslint/no-empty-function': 'warn',
         'react/display-name': 'warn',
-        'simple-import-sort/sort': 'error',
         'sort-imports': 'off',
         'import/order': 'off',
     },
