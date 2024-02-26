d02dda4278d8653db3df692dec3bd98fcb8b3d69
shaneajeffery <shane.jeffery@mckesson.com>
2024-02-26 09:24:57 -0500

    Bootstrap astro project

--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './assets/css/styles.css';
-import App from './app';
-
-ReactDOM.render(<App />, document.getElementById('root'));
